631566f6f7f07b21b05cfb46ec823bc0fd412c57
Aleksei Lobankov <alekseilobankov@icloud.com>
2024-10-08 17:58:52 +0200

    .zshenv variables retrieval, encoding and decoding done. Removing key works not very smooth. A .env file that is added to the repo and then removed from the source control might be a better solution.

--- TMDBuddySecrets/Sources/GenerateEnvironmentalVariablesExec/main.swift
+++ TMDBuddySecrets/Sources/GenerateEnvironmentalVariablesExec/main.swift
@@ -5,43 +5,83 @@ guard arguments.count >= 2 else {
     throw GeneratedEnvironmentVariablesError.invalidArguments
 }
 
-let outputURL = URL(filePath: arguments[1]).appending(path: "GeneratedEnvironmentVariables.swift")
+try generateSwiftCode(outputURL: arguments[1])
 
-let process = Process()
-process.executableURL = URL(fileURLWithPath: "/bin/zsh")
-process.arguments = ["-c", "echo $TMDBUDDY_API_KEY"]
+// MARK: - Functions
 
-let outputPipe = Pipe()
-process.standardOutput = outputPipe
-try process.run()
-process.waitUntilExit()
+func generateSwiftCode(outputURL: String) throws {
+    let apiKey = try retrieve(environmentVariable: .apiKey)
+    let salt = try generateRandomSalt()
+    let obfuscatedApiKey = try obfuscate(key: apiKey, salt: salt)
 
-let apiKey: String
+    let code = """
+    /// Generated environment variables from .zshenv file.
+    /// Do not modify manually.
 
-guard
-    let outputData = try outputPipe.fileHandleForReading.readToEnd(),
-    let apiKey = String(data: outputData, encoding: .utf8)?.trimmingCharacters(in: .whitespacesAndNewlines),
-    !apiKey.isEmpty
-else {
-    throw GeneratedEnvironmentVariablesError.missingKey
+    enum GeneratedEnvironmentVariables {
+        static let apiKey: [UInt8] = \(obfuscatedApiKey)
+    
+        static let salt: [UInt8] = \(salt)
+    }
+    """
+
+    guard let data = code.data(using: .utf8) else {
+        throw GeneratedEnvironmentVariablesError.invalidData
+    }
+
+    let outputURL = URL(filePath: outputURL).appending(path: "GeneratedEnvironmentVariables.swift")
+    try data.write(to: outputURL, options: .atomic)
 }
 
-let code = """
-/// Generated from environment variables from .zshenv file.
+func retrieve(environmentVariable key: EnvironmentKeys) throws -> String {
+    let process = Process()
+    process.executableURL = URL(fileURLWithPath: "/bin/zsh")
+    process.arguments = ["-c", "echo $\(key.rawValue)"]
 
-enum GeneratedEnvironmentVariables {
-    static let apiKey = "\(apiKey)"
+    let outputPipe = Pipe()
+    process.standardOutput = outputPipe
+    try process.run()
+    process.waitUntilExit()
+
+    guard
+        let outputData = try outputPipe.fileHandleForReading.readToEnd(),
+        let environmentVariable = String(data: outputData, encoding: .utf8)?.trimmingCharacters(in: .whitespacesAndNewlines),
+        !environmentVariable.isEmpty
+    else {
+        throw GeneratedEnvironmentVariablesError.missingKey(key.rawValue)
+    }
+
+    return environmentVariable
 }
-"""
 
-guard let data = code.data(using: .utf8) else {
-    throw GeneratedEnvironmentVariablesError.invalidData
+func generateRandomSalt() throws(GeneratedEnvironmentVariablesError) -> [UInt8] {
+    let length = Int.random(in: 16...64)
+    var salt = [UInt8](repeating: 0, count: length)
+    let result = SecRandomCopyBytes(kSecRandomDefault, length, &salt)
+    guard result == errSecSuccess else {
+        throw GeneratedEnvironmentVariablesError.saltGenerationFailed
+    }
+    return salt
 }
 
-try data.write(to: outputURL, options: .atomic)
+func obfuscate(key: String, salt: [UInt8]) throws(GeneratedEnvironmentVariablesError) -> [UInt8] {
+    key.utf8
+        .enumerated()
+        .map { index, byte in
+            byte ^ salt[index % salt.count]
+        }
+}
+
+// MARK: - Utility types
+
+enum EnvironmentKeys: String {
+    case apiKey = "TMDBUDDY_API_KEY"
+}
 
 enum GeneratedEnvironmentVariablesError: Error {
     case invalidArguments
-    case missingKey
+    case missingKey(String)
+    case saltGenerationFailed
+    case obfuscationFailed
     case invalidData
 }
